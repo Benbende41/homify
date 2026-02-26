13eb93955ebd32f97ffeb8e23075dce60ca54ae7
lihaibo <15039069378@163.com>
2026-02-26 17:16:42 +0800

    feat: ✨  init

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import VueEslintParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
+export default [
+    {
+        files: ['**/*.{ts,tsx,vue}'],
+        plugins: {
+            vue: pluginVue,
+            'simple-import-sort': simpleImportSort
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...pluginVue.configs['flat/recommended'].rules,
+            'simple-import-sort/imports': 'error'
+        },
+        languageOptions: {
+            parser: VueEslintParser,
+            parserOptions: {
+                parser: tsParser
+            }
+        }
+    }
+]
